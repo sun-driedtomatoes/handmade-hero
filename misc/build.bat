3c214c9b9bad0c9b0444f0d81e1f3d264971f8c9
sun-driedtomatoes <68627568+sun-driedtomatoes@users.noreply.github.com>
2021-02-16 19:39:06 -0500

    Add files via upload

---
+++ misc/build.bat
@@ -0,0 +1,18 @@
+  
+@echo off
+if not exist w:\build mkdir w:\build
+pushd w:\build
+
+set CommonCompilerFlags=-nologo -FC -WX -W4 -wd4201 -wd4100 -wd4505 -wd4189 -wd4457 -MTd -Oi -Od -GR- -Gm- -EHa- -Zi -DHANDMADE_SLOW=1 -DHANDMADE_INTERNAL=1 -DHANDMADE_WIN32=1
+set CommonLinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+
+rem 32-bit build
+rem cl %CommonCompilerFlags% w:\handmade\code\win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
+
+rem 64-bit build
+del *.pdb > NUL 2> NUL
+REM Optimization switches: /O2 /Oi /fp:fast
+cl %CommonCompilerFlags% -Fmwin32_handmade.map w:\handmade\code\win32_handmade.cpp /link %CommonLinkerFlags%
+echo Build Lock > lock.tmp
+del lock.tmp
+popd
